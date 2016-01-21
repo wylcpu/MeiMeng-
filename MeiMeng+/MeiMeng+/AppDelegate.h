4aa2c86ac5c992f4f494cbebf239227401f7672e
wylcpu <wylcpu@163.com>
2016-01-21 09:50:28 +0800

    创建项目

---
+++ MeiMeng+/MeiMeng+/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiMeng+
+//
+//  Created by eviloo7 on 16/1/21.
+//  Copyright © 2016年 BlueOcean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiMeng+/MeiMeng+/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiMeng+
+//
+//  Created by eviloo7 on 16/1/21.
+//  Copyright © 2016年 BlueOcean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
